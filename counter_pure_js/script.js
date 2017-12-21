04b85612fc25d07faff48c9866a1677e3396a760
Xilin Sun <s.sn.giraffe@gmail.com>
2017-12-20 18:52:03 -0800

    update pure js counter: use single 'reducer' function

--- counter_pure_js/script.js
+++ counter_pure_js/script.js
@@ -4,13 +4,10 @@
 // other Flux implementations might have multiple stores
 let state = 0
 
-// this will be replaced by a reducer
-const onIncrement = () => {
-  state = state + 1
-  render()
-}
-const onDecrement = () => {
-  state = state - 1
+const updateState = (inc) => {
+  // the line below will be replaced by a reducer
+  state += inc
+  
   render()
 }
 
@@ -19,8 +16,8 @@ const render = () => {
   document.getElementById('root').innerHTML = `
     <div class="app">
       <h1 id="state">${state}</h1>
-      <button onClick="onIncrement()"> + </button>
-      <button onClick="onDecrement()"> - </button>
+      <button onClick="updateState(1)"> + </button>
+      <button onClick="updateState(-1)"> - </button>
     </div>    
   `
 }
