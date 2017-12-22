ec68258eecb355d518eae4e46820f87e9d59cc75
Xilin Sun <s.sn.giraffe@gmail.com>
2017-12-21 18:38:45 -0800

    add counter list app

---
+++ counter_list_pure_js/script.js
@@ -0,0 +1,82 @@
+// Counter list app using Vanilla JavaScript
+
+const addCounter = (list) => {
+  return [...list, 0];
+};
+
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index + 1)
+  ];
+};
+
+const incrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+};
+
+const decrementCounter = (list, index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] - 1,
+    ...list.slice(index + 1)
+  ];
+};
+
+let list = [0, 1, 2]
+
+// reducer
+const counter = (list = [], action) => {
+  switch (action.type) {
+    case 'ADD':
+      return addCounter(list)
+    case 'REMOVE':
+      return removeCounter(list, action.index)
+    case 'INCREMENT':
+      return incrementCounter(list, action.index)
+    case 'DECREMENT':
+      return decrementCounter(list, action.index)
+    default:
+      return list
+  }
+}
+
+const updateState = (action) => {
+  list = counter(list, action)
+  render()
+}
+
+// this will be replaced by ReactDOM.render(), React components and actions
+const render = () => {
+  document.getElementById('root').innerHTML = `
+    <div class="control">
+      <button onClick="updateState({'type': 'ADD'})">
+        Add Counter
+      </button>
+    </div>
+  `
+  list.forEach((value, i) => {
+    document.getElementById('root').innerHTML += `
+      <div class="app">
+        <h1 id="state">${value}</h1>
+        <button onClick="updateState({'type': 'DECREMENT', 'index': ${i}})">
+          -
+        </button>
+        <button onClick="updateState({'type': 'INCREMENT', 'index': ${i}})">
+          +
+        </button>
+        <button onClick="updateState({'type': 'REMOVE', 'index': ${i}})">
+          x
+        </button>
+      </div>
+    `
+  })
+}
+
+window.onload = () => {
+  render()
+}
