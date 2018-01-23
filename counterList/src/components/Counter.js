c9fa85519c9c2090f3b3971f6345fce79517bdde
Xilin Sun <s.sn.giraffe@gmail.com>
2018-01-22 18:38:04 -0800

    add counterList

---
+++ counterList/src/components/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { increaseValue, resetValue } from '../actions'
+
+const Counter = ({value, onIncrease, onDecrease, onReset}) => (
+  <div className="counter">
+    <p>React Redux Counter</p>
+    <h1>{value}</h1>
+    <button onClick={onDecrease}>-</button>
+    <button onClick={onIncrease}>+</button>
+    <button onClick={onReset}>RESET</button>
+  </div>
+)
+
+const mapStateToProps = state => ({
+  value: state.value
+})
+
+const mapDispatchToProps = dispatch => ({
+  onIncrease: () => dispatch(increaseValue(1)),
+  onDecrease: () => dispatch(increaseValue(-1)),
+  onReset: () => dispatch(resetValue())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
